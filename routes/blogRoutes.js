50d8b4baf1b10660574e53f019b158b196c6f2d6
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-08-26 23:40:56 +0300

    feat: Add view counter routes - /popular and /:blogId/view endpoints

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -11,12 +11,16 @@ router.get("/", optionalAuth, blogController.getAllBlogs);
 router.get("/category/:categoryId", optionalAuth, blogController.getBlogsByCategory);
 router.get("/author/:authorId", optionalAuth, blogController.getBlogsByAuthor);
 router.get("/search", optionalAuth, blogController.searchBlogs);
+router.get("/popular", optionalAuth, blogController.getPopularBlogs);
 router.get("/:id", optionalAuth, blogController.getBlogById);
 
 // Protected routes (authentication gerekir)
 router.post("/", authenticateToken, validateBlog, blogController.createBlog);
 router.put("/:id", authenticateToken, validateBlogUpdate, blogController.updateBlog);
 router.delete("/:id", authenticateToken, blogController.deleteBlog);
+
+// View counter artırma (public route)
+router.post("/:blogId/view", blogController.incrementViewCount);
 
 // Comment rotaları nested olarak
 router.use("/:blogId/comments", commentRoutes);
